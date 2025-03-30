6b18b6ee94f1017d9fa9e86ec5c741be8ae30eb3
Jin <jinojoycomillas20@gmail.com>
2025-03-30 19:26:49 +0800

    chore: Add a dummy record in seeder

--- db/seeder.ts
+++ db/seeder.ts
@@ -1,5 +1,5 @@
 import drizzle from "./drizzle";
-import { eWalletsTable } from "./schema";
+import { eWalletsTable, recordsTable } from "./schema";
 
 async function eWalletsSeeder() {
   await drizzle
@@ -9,6 +9,15 @@ async function eWalletsSeeder() {
       cellNumber: "09215177647",
     })
     .onConflictDoNothing({ target: eWalletsTable.id });
+
+  await drizzle.insert(recordsTable).values({
+    referenceNumber: "1234567890123",
+    cellNumber: "09215177647",
+    amount: 1000,
+    fee: 100,
+    type: "cash-in",
+    date: new Date(),
+  });
 
   console.log("Seeder loaded successfully");
 }
