aeefe588db2afb880814b3eda98def2154078bad
Jin <jinojoycomillas20@gmail.com>
2025-03-30 22:20:37 +0800

    chore: Update seeder

--- db/seeder.ts
+++ db/seeder.ts
@@ -8,16 +8,19 @@ async function eWalletsSeeder() {
       name: "G-cash",
       cellNumber: "09215177647",
     })
-    .onConflictDoNothing({ target: eWalletsTable.id });
+    .onConflictDoNothing({ target: eWalletsTable.name });
 
-  await drizzle.insert(recordsTable).values({
-    referenceNumber: "1234567890123",
-    cellNumber: "09215177647",
-    amount: 1000,
-    fee: 100,
-    type: "cash-in",
-    date: new Date(),
-  });
+  await drizzle
+    .insert(recordsTable)
+    .values({
+      referenceNumber: "1234567890123",
+      cellNumber: "09215177647",
+      amount: 1000,
+      fee: 100,
+      type: "cash-in",
+      date: new Date(),
+    })
+    .onConflictDoNothing({ target: recordsTable.referenceNumber });
 
   console.log("Seeder loaded successfully");
 }
