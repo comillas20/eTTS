8a37da5ab9b6156a32a7c526ca04afdc42c1c814
Jin <jinojoycomillas20@gmail.com>
2025-05-01 17:27:26 +0800

    chore: Add status column + display money properly

--- app/(dashboard)/(routes)/[wallet]/components/datatable/columns.tsx
+++ app/(dashboard)/(routes)/[wallet]/components/datatable/columns.tsx
@@ -1,14 +1,15 @@
 import { ColumnDef } from "@tanstack/react-table";
 import { Record } from "../../actions";
+import { Badge } from "@/components/ui/badge";
 
 export const columns: ColumnDef<Record>[] = [
   {
-    id: "Reference",
+    id: "reference",
     accessorKey: "referenceNumber",
     header: "Reference",
   },
   {
-    id: "Mobile number",
+    id: "mobile number",
     accessorKey: "cellNumber",
     header: "Mobile number",
   },
@@ -19,13 +20,42 @@ export const columns: ColumnDef<Record>[] = [
   {
     accessorKey: "amount",
     header: "Amount",
+    cell: ({ row }) =>
+      Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "PHP",
+        currencyDisplay: "code",
+      }).format(row.original.amount),
   },
   {
     accessorKey: "fee",
     header: "Fee",
+    cell: ({ row }) =>
+      Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "PHP",
+        currencyDisplay: "code",
+      }).format(row.original.fee),
   },
   {
     accessorKey: "date",
     header: "Date",
+  },
+  {
+    id: "status",
+    accessorKey: "claimedAt",
+    header: "Status",
+    cell: ({ row }) => {
+      const value = row.original.claimedAt;
+
+      return value ? (
+        <div className="flex gap-2">
+          <Badge variant="secondary">Claimed</Badge>
+          <span>{value}</span>
+        </div>
+      ) : (
+        <Badge>Unclaimed</Badge>
+      );
+    },
   },
 ];
