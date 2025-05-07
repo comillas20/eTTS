2611927850dc2b37889908cb48b1119ca62f923b
Jino Joy C. Comillas <jinojoycomillas20@gmail.com>
2025-05-07 12:45:01 +0800

    chore: Use meta props on datatable

--- app/(dashboard)/(routes)/[wallet]/components/datatable/frame.tsx
+++ app/(dashboard)/(routes)/[wallet]/components/datatable/frame.tsx
@@ -17,39 +17,57 @@ export function Frame({ table }: FrameProps) {
     <div className="overflow-hidden rounded-lg border md:min-h-min">
       <Table>
         <TableHeader className="bg-primary">
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow
-              key={headerGroup.id}
-              className="text-primary-foreground hover:bg-inherit">
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext(),
-                        )}
-                  </TableHead>
-                );
-              })}
-            </TableRow>
-          ))}
+          {table.getHeaderGroups().map((headerGroup) => {
+            const meta = table.options.meta?.headerRowProps;
+            let props: React.ComponentProps<"tr"> | undefined;
+            if (meta) props = meta(headerGroup);
+            return (
+              <TableRow
+                key={headerGroup.id}
+                className="text-primary-foreground hover:bg-inherit"
+                {...props}>
+                {headerGroup.headers.map((header) => {
+                  const meta = header.column.columnDef.meta?.columnHeaderProps;
+                  let props: React.ComponentProps<"th"> | undefined;
+                  if (meta) props = meta(header);
+                  return (
+                    <TableHead key={header.id} {...props}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            );
+          })}
         </TableHeader>
         <TableBody>
           {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && "selected"}
-                className="data-[state=selected]:bg-primary/50">
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id} className="capitalize">
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))
+            table.getRowModel().rows.map((row) => {
+              const meta = table.options.meta?.bodyRowProps;
+              let props: React.ComponentProps<"tr"> | undefined;
+              if (meta) props = meta(row);
+              return (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                  className="data-[state=selected]:bg-primary/50"
+                  {...props}>
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id} className="capitalize">
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              );
+            })
           ) : (
             <TableRow>
               <TableCell
