c3df08b4912c9b5f00da6d012f4365295e997c72
Jin <jinojoycomillas20@gmail.com>
2025-05-15 12:17:43 +0800

    feat: Add actually functional wallet form

--- app/(dashboard)/(routes)/create/components/wallet-form.tsx
+++ app/(dashboard)/(routes)/create/components/wallet-form.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -12,12 +13,14 @@ import {
 import { Input } from "@/components/ui/input";
 import { eWalletsTable } from "@/db/schema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createInsertSchema } from "drizzle-zod";
+import { PlusIcon, XIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import { z } from "zod";
 import { createWallet } from "../actions";
-import { toast } from "sonner";
-import { useRouter } from "next/navigation";
 
 const formSchema = createInsertSchema(eWalletsTable, {
   name: (schema) => schema.min(1, "E-wallet name is required"),
@@ -41,22 +44,29 @@ export function WalletForm({ wallet }: WalletFormProps) {
 
   const router = useRouter();
 
+  const queryClient = useQueryClient();
+  const walletM = useMutation({
+    mutationFn: createWallet,
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ["e-wallets"] });
+
+      if (data instanceof Error) toast.error(data.message);
+      else
+        toast("Wallet has been created successfully", {
+          action: { label: "View", onClick: () => router.push(`/${data.url}`) },
+        });
+    },
+  });
+
   const onSubmit = async (values: WalletForm) => {
-    const result = await createWallet(values);
-    if (result instanceof Error) {
-      toast.error(result.message);
-    } else {
-      toast("A record has been created", {
-        action: { label: "View", onClick: () => router.push(`/${result.url}`) },
-      });
-    }
+    walletM.mutate(values);
   };
 
   return (
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className="max-w-96 space-y-4">
+        className="max-w-[36rem] space-y-8">
         <FormField
           control={form.control}
           name="name"
@@ -98,6 +108,22 @@ export function WalletForm({ wallet }: WalletFormProps) {
             </FormItem>
           )}
         />
+        <div className="flex items-center justify-end gap-4">
+          <Button
+            type="button"
+            variant="outline"
+            onClick={() => router.back()}
+            disabled={form.formState.isSubmitting}>
+            <XIcon />
+            Cancel
+          </Button>
+          <Button
+            type="submit"
+            disabled={!form.formState.isDirty || form.formState.isSubmitting}>
+            <PlusIcon />
+            Create
+          </Button>
+        </div>
       </form>
     </Form>
   );
