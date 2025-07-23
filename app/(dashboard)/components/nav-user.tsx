e713ff88bcab6949cd654d820fbe8dc24c3705bf
Jin <jinojoycomillas20@gmail.com>
2025-07-23 19:35:20 +0800

    style: Improve design

--- app/(dashboard)/components/nav-user.tsx
+++ app/(dashboard)/components/nav-user.tsx
@@ -13,6 +13,7 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { authClient } from "@/lib/auth-client";
 import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
 
 export function NavUser() {
   const { data: session, isPending } = authClient.useSession();
@@ -25,14 +26,16 @@ export function NavUser() {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger className="relative">
-        <Avatar className="rounded-lg">
-          {user.image && <AvatarImage src={user.image} alt={user.name} />}
-          <AvatarFallback className="rounded-lg">
-            {user.name.charAt(0).toUpperCase()}
-          </AvatarFallback>
-        </Avatar>
-        <ChevronDownCircleIcon className="fill-primary absolute -right-1 -bottom-1 size-4 stroke-white" />
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon" className="relative p-px">
+          <Avatar className="size-full rounded-sm">
+            {user.image && <AvatarImage src={user.image} alt={user.name} />}
+            <AvatarFallback className="rounded-none">
+              {user.name.charAt(0).toUpperCase()}
+            </AvatarFallback>
+          </Avatar>
+          <ChevronDownCircleIcon className="fill-primary absolute -right-1 -bottom-1 size-4 stroke-white" />
+        </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="rounded-lg" align="end" sideOffset={4}>
         <DropdownMenuLabel className="p-0 font-normal">
