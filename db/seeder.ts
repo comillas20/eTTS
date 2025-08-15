d3ed4bae0e7acce310413b7781a64c3d7b2a7e38
Jin <jinojoycomillas20@gmail.com>
2025-08-15 18:09:19 +0800

    chore: Update seeder

--- db/seeder.ts
+++ db/seeder.ts
@@ -1,40 +1,24 @@
 import db from "./drizzle";
-import { eWalletsTable, recordsTable } from "./schema";
+import { eWalletsTable } from "./schema";
 
-async function eWalletsSeeder() {
-  const eWallet = await db
+type eWALLET = typeof eWalletsTable.$inferInsert;
+const eWallets: eWALLET[] = [
+  {
+    name: "G-cash",
+    url: "g-cash",
+    cellNumber: "09165591258",
+  },
+  // Add more e-wallets here
+];
+
+export async function main() {
+  await db
     .insert(eWalletsTable)
-    .values({
-      name: "G-cash",
-      url: "g-cash",
-      cellNumber: "09215177647",
-    })
+    .values(eWallets)
     .returning({ eWalletId: eWalletsTable.id })
     .onConflictDoNothing({ target: eWalletsTable.name });
-
-  await db.insert(recordsTable).values([
-    {
-      referenceNumber: "1234567890123",
-      cellNumber: "09215177647",
-      amount: 1000,
-      fee: 20,
-      type: "cash-in",
-      date: new Date(),
-      eWalletId: eWallet[0].eWalletId,
-    },
-    {
-      referenceNumber: "1234567890455",
-      cellNumber: "09215177647",
-      amount: 10000,
-      fee: 200,
-      type: "cash-in",
-      date: new Date(),
-      eWalletId: eWallet[0].eWalletId,
-      claimedAt: new Date(),
-    },
-  ]);
 
   console.log("Seeder loaded successfully");
 }
 
-eWalletsSeeder();
+main();
