8b970ba4baed74107c195a7c7162f1c2212e2564
Jin <jinojoycomillas20@gmail.com>
2025-08-23 17:32:46 +0800

    build: Update py scripts and commands

---
+++ scripts/dep-installations/py-install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Running Python dependency installer..."
+
+# Activate the virtual environment
+source .venv/bin/activate
+
+# Check if activation was successful
+if [ $? -ne 0 ]; then
+    echo ""
+    echo "Error: Failed to activate virtual environment."
+    exit 1
+fi
+
+# Use the "$@" variable to install all packages passed as arguments
+python -m pip install "$@"
+if [ $? -ne 0 ]; then
+    echo ""
+    echo "Error: Failed to install packages."
+    exit 1
+fi
+
+# Update the requirements.txt file
+python -m pip freeze > requirements.txt
+echo ""
+echo "Successfully updated requirements.txt."
